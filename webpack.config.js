5323197b94840a7241b00db787869884a4f11c97
alexanderwiederin <alexander.wiederin@gmail.com>
2018-10-23 18:24:44 -0700

    completed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,12 @@ module.exports = {
     path: DIST_DIR
   },
   module : {
-    loaders : [
+    rules : [
       {
         test : /\.jsx?/,
         include : SRC_DIR,
         loader : 'babel-loader',      
-        query: {
+        options: {
           presets: ['@babel/preset-env', '@babel/preset-react']
         }
       }
