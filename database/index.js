7403d223eabbef8462fe34ad28668ed9114e291e
alexanderwiederin <alexander.wiederin@gmail.com>
2018-10-23 19:27:45 -0700

    configured database

--- database/index.js
+++ database/index.js
@@ -1,1 +1,20 @@
-index.js
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/nike');
+var Schema = mongoose.Schema;
+
+var reviewSchema = new Schema({
+  shoeID: String,
+  author: String,
+  title: String,
+  stars: {
+    type: Number,
+    min: 0,
+    max: 5
+  },
+  body: String,
+  createdAt: Date
+})
+
+var Review = mongoose.model("Review", reviewSchema);
+
+module.exports.Review = Review;
--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-index.js
+var express = require('express');
+var parseBody = require('body-parser');
+var path = require('path');
+var Review = require('../database/index.js');
+var PORT = 3000;
+
+var app = express();
+
+app.use(express.static(path.join(__dirname + 'client/dist/')));
+app.use(parseBody.json());
+
+app.listen(PORT, () => {
+  console.log('listening on port: ' + PORT);
+})
+
+
