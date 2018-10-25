b1cb412b8b00e74688355c836f10fb4419349b20
alexanderwiederin <alexander.wiederin@gmail.com>
2018-10-25 16:19:46 -0700

    got first test to work

--- server/database/test.js
+++ server/database/test.js
@@ -1,5 +1,15 @@
 var seedFunction = require('./newSeed.js');
+var dbMethods = require('./Review.js');
 
 test('creates array of reviews', () => {
-  expect(Array.isArray(seedFunction.createDummyReviews())).toBeTruthy();
+  var dummyReviews = seedFunction.createDummyReviews();
+  var dummyReviewKeys = Object.keys(dummyReviews[0]);
+  console.log('dummyReviewKeys' ,dummyReviewKeys);
+  var requiredProperties = ['shoeID', 'author', 'title', 'stars', 'body', 'createdAt'];
+
+  expect(Array.isArray(dummyReviews)).toBeTruthy();
+  expect(typeof dummyReviews[0]).toEqual('object');
+  expect(requiredProperties.every((property) => {
+    return dummyReviewKeys.includes(property);
+  })).toEqual(true);
 });
