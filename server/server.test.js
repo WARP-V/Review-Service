55b6865daa7129bf663fe0bca9a377b6267ad249
alexanderwiederin <alexander.wiederin@gmail.com>
2018-10-26 19:28:16 -0700

    first server test working

--- server/server.test.js
+++ server/server.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const api = require('./api');
 
-test('should respond to the initial get request', () => {
+test('should respond to the initial get request', (done) => {
   request(api).get('/').then((response) => {
     expect(response.statusCode).toBe(200);
     done();
