b44d47bf41a1a78c9bb6ab233f626899b7048725
alexanderwiederin <alexander.wiederin@gmail.com>
2018-10-29 15:48:55 -0700

    add working app component

---
+++ client/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      reviews: [],
+    };
+  }
+
+  render() {
+    return (
+      <div>Rendering App worked</div>
+    );
+  }
+}
+
+export default App;
