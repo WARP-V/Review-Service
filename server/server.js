d2072a75102b08a1cab91d7c5f431654c3426d04
alexanderwiederin <alexander.wiederin@gmail.com>
2018-11-08 22:01:17 -0800

    refactor server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const api = require('./api.js');
+require('./database/newSeed.js');
 
 const PORT = 3004;
 
 api.listen(PORT, () => console.log(`listening on port: ${PORT}`));
 
-require('./database/newSeed.js');
+
